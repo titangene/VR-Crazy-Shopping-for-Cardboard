650cfb78ddf73aaec40d0ac760fe362bf05b296c
titangene <titangene01@gmail.com>
2016-07-09 23:10:19 +0800

    updated Test1.unity, MovingController.cs This version cart and player can run normal.

--- Market/Scripts/MovingController.cs
+++ Market/Scripts/MovingController.cs
@@ -83,7 +83,9 @@ public class MovingController : MonoBehaviour {
         if (HoldTrigger) {
             // 準心對準購物車手把時 gaze.IsHeld() = true，沒有準心對準時 = false
             if (gaze.IsHeld() && gaze.Object().name == "Handle") {
-                Debug.Log("Moving");
+                //Debug.Log("Moving");
+                // 準心對準購物車手把 狀態改成 true
+                GazeCart = true;
                 // 將 向前移動 狀態改成 true，玩家和購物車同時向前移動
                 MoveForward = true;
             }
@@ -92,13 +94,13 @@ public class MovingController : MonoBehaviour {
 
     // 玩家和購物車同時停止向前移動
     private void StopMove(object sender) {
-        Debug.Log("Stop");
+        //Debug.Log("Stop");
         // 將 向前移動 狀態改成 false，玩家和購物車同時停止向前移動
         MoveForward = false;
     }
 
-    // 玩家和購物車同時向前移動
-    private void Move() {
+    // 玩家向前移動
+    private void PlayerMove() {
         // 準心對準購物車的手把並按住按鈕即可往前移動，放開按鈕可停止移動
         // 玩家同時能透過移動頭部方向來改變移動方位
         
@@ -106,30 +108,17 @@ public class MovingController : MonoBehaviour {
         Vector3 forward = Camera.main.transform.forward;
         // 讓角色往前
         controller.SimpleMove(forward * speed);
-
-        
     }
 
-    void Update() {
-        // 是否按下 Gvr 按鈕
-        if (cardboard.trigger.IsHeld()) {
-            // 檢查是否按住 Gvr 按鈕
-            CheckTrigger();
-        } else {
-            // 將 按住 Gvr 按鈕 狀態改成 false
-            HoldTrigger = false;
-        }
-
-        // 向前移動 狀態 = true，玩家和購物車同時向前移動
-        if (MoveForward) {
-            // 玩家和購物車同時向前移動
-            Move();
-        }
-
+    // 購物車跟著玩家移動
+    private void CartMove() {
         // 更新購物車與人物角色的距離
         Cart_Player_Distance = Cart.position - transform.position;
         // 紀錄購物車與人物角色的距離 (r 圓型半徑)   畢氏定理 C = sqrt(A^2 + B^2)
         Cart_Player = Mathf.Sqrt(Mathf.Pow(Cart_Player_Distance.x, 2) + Mathf.Pow(Cart_Player_Distance.z, 2));
+        if (Cart_Player != 1.5f) {
+            Cart_Player = 1.5f;
+        }
         // 攝影機 Y 軸旋轉角度 (最高只能 90 度，用於theta)
         float Camera_AngleY = Head.transform.eulerAngles.y;
         // Mathf.Deg2Rad 度轉弧度 = (PI * 2) / 360
@@ -139,8 +128,29 @@ public class MovingController : MonoBehaviour {
         // 紀錄購物車 Z 座標：z = r * cos(thita)
         Cart_Z = Cart_Player * Mathf.Cos(theta);
         // 購物車會跟著玩家的視角移動位置
-        Cart.position = new Vector3(Cart_X, 0f, Cart_Z);
+        Cart.position = new Vector3(Cart_X + transform.position.x, 0f, Cart_Z + transform.position.z);
         // 購物車會跟著玩家的視角旋轉角度
         Cart.rotation = Quaternion.Euler(0, Camera_AngleY, 0);
+    }
+
+    void Update() {
+        // 是否按下 Gvr 按鈕
+        if (cardboard.trigger.IsHeld()) {
+            // 檢查是否按住 Gvr 按鈕
+            CheckTrigger();
+        } else {
+            // 準心對準購物車手把 狀態改成 false
+            GazeCart = false;
+            // 將 按住 Gvr 按鈕 狀態改成 false
+            HoldTrigger = false;
+        }
+
+        // 向前移動 狀態 = true，玩家和購物車同時向前移動
+        if (MoveForward) {
+            // 玩家向前移動
+            PlayerMove();
+            // 購物車跟著玩家移動
+            CartMove();
+        }
     }
 }
