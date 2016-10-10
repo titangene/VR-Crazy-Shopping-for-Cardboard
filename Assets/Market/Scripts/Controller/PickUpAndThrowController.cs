b80dba2e9efdc452a022553bb017ee7fe515878a
titangene <titangene01@gmail.com>
2016-10-11 00:44:33 +0800

    修改：重購、新增GCvrTrigger OnClick，Print Product IsOverRange、Debug GCvrGaze -> null

--- Assets/Market/Scripts/Controller/PickUpAndThrowController.cs
+++ Assets/Market/Scripts/Controller/PickUpAndThrowController.cs
@@ -2,13 +2,17 @@
 
 public class PickUpAndThrowController : MonoBehaviour {
     /// <summary>
+    /// Debug gazeObj：拿取商品時，準心會永遠放大，丟出商品後，準心會縮小
+    /// </summary>
+    public GameObject FixGazeObj;
+    /// <summary>
     /// 購物車的碰撞體
     /// </summary>
     public GameObject Cart_Collider;
     /// <summary>
-    /// Debug gazeObj：拿取商品時，準心會永遠放大，丟出商品後，準心會縮小
+    /// 商品被拿取時的位置
     /// </summary>
-    public GameObject FixGazeObj;
+    public Transform PickupObj;
     /// <summary>
     /// 是否拿取商品
     /// </summary>
@@ -27,6 +31,8 @@ public class PickUpAndThrowController : MonoBehaviour {
     public float Throw_Power = 3.0f;
 
     private GCvrGaze GCvrGaze;
+    private GCvrTrigger GCvrTrigger;
+    private Camera cam;
     /// <summary>
     /// 目標物件 (商品)
     /// </summary>
@@ -35,17 +41,14 @@ public class PickUpAndThrowController : MonoBehaviour {
     /// 目標物件之剛體 (商品)
     /// </summary>
     private Rigidbody HitRB = null;
-    /// <summary>
-    /// 商品被拿取時的位置
-    /// </summary>
-    private Transform PickupObj;
+
+    private static PickUpAndThrowController instance = null;
 
     public static PickUpAndThrowController Instance {
         get {
 #if UNITY_EDITOR
-            if (instance == null && !Application.isPlaying) {
+            if (instance == null && !Application.isPlaying)
                 instance = UnityEngine.Object.FindObjectOfType<PickUpAndThrowController>();
-            }
 #endif
             if (instance == null) {
                 Debug.LogError("No PickUpAndThrowController instance found.  Ensure one exists in the scene, or call "
@@ -60,7 +63,6 @@ public class PickUpAndThrowController : MonoBehaviour {
             return instance;
         }
     }
-    private static PickUpAndThrowController instance = null;
 
     public static void Create() {
         if (instance == null && UnityEngine.Object.FindObjectOfType<PickUpAndThrowController>() == null) {
@@ -72,9 +74,8 @@ public class PickUpAndThrowController : MonoBehaviour {
     }
 
     void Awake() {
-        if (instance == null) {
+        if (instance == null)
             instance = this;
-        }
         if (instance != this) {
             Debug.LogError("There must be only one PickUpAndThrowController object in a scene.");
             UnityEngine.Object.DestroyImmediate(this);
@@ -83,56 +84,69 @@ public class PickUpAndThrowController : MonoBehaviour {
     }
 
     void Start() {
-        GCvrGaze = Camera.main.GetComponent<GCvrGaze>();
-        PickupObj = GameObject.Find("PickupObject").transform;
+        cam = Camera.main;
+        GCvrGaze = cam.GetComponent<GCvrGaze>();
+        GCvrTrigger = cam.GetComponent<GCvrTrigger>();
+
+        // Gvr 按鈕事件
+        GCvrTrigger.OnClick += GCvrClick;
     }
 
     void LateUpdate() {
         // 是否按一下 Gvr 按鈕拿取商品
-        if (PickingUp) {
+        if (PickingUp)
             // 商品跟著玩家頭部方向移動
             PickUpProduct();
-        }
+    }
+
+    private void GCvrClick(object sender) {
+        // 商品與人物角色的距離超過設定範圍
+        if (GCvrGaze.CurrentObj_Infinity() && 
+            GCvrGaze.GetObjName(GCvrGaze.CurrentObj_Infinity()).Contains("Pro_Obj"))
+            if (GCvrGaze.IsOverRange())
+                Debug.Log("商品超過可拿取範圍");
+        
     }
 
     /// <summary>
     /// 在設定時間內按下並且快速放開 Gvr 按鈕間，會判定為點擊事件觸發
     /// </summary>
     public void CheckClick() {
-        // 按第一次 Gvr 按鈕：拿取商品
-        if (!CanSecondClick) {
-            // 設定目標物件 (商品)
-            TargetObj = GCvrGaze.GetCurrentObj_Range().transform;
-            // 設定目標物件之剛體 (商品)
-            HitRB = GCvrGaze.GetHit().rigidbody;
-            Debug.Log("PickUp：" + TargetObj.name);
+        if (GCvrGaze.CurrentObj_Range() != null) {
+            // 按第一次 Gvr 按鈕：拿取商品
+            if (!CanSecondClick) {
+                // 設定目標物件 (商品)
+                TargetObj = GCvrGaze.CurrentObj_Range().transform;
+                // 設定目標物件之剛體 (商品)
+                HitRB = GCvrGaze.Hit_Range().rigidbody;
+                Debug.Log("PickUp：" + TargetObj.name);
 
-            // 將 是否拿取商品 狀態改成 true
-            PickingUp = true;
-            // 將 是否能按第二次 Gvr 按鈕 狀態改成 true
-            CanSecondClick = true;
-            // 將 Cart_Collider、FixGazeObj 物件開啟
-            Cart_Collider.SetActive(true);
-            FixGazeObj.SetActive(true);
+                // 將 是否拿取商品 狀態改成 true
+                PickingUp = true;
+                // 將 是否能按第二次 Gvr 按鈕 狀態改成 true
+                CanSecondClick = true;
+                // 將 Cart_Collider、FixGazeObj 物件開啟
+                Cart_Collider.SetActive(true);
+                FixGazeObj.SetActive(true);
 
-            // 按第二次 Gvr 按鈕：丟出商品
-        } else {
-            Debug.Log("Throw");
+                // 按第二次 Gvr 按鈕：丟出商品
+            } else {
+                Debug.Log("Throw");
+                // 將 是否拿取商品 狀態改成 false
+                PickingUp = false;
+                // 將 是否能按第二次 Gvr 按鈕 狀態改成 false
+                CanSecondClick = false;
+                // 丟出商品
+                ThrowProduct();
+                // 將 Cart_Collider、FixGazeObj 物件關閉
+                Cart_Collider.SetActive(false);
+                FixGazeObj.SetActive(false);
 
-            // 將 是否拿取商品 狀態改成 false
-            PickingUp = false;
-            // 將 是否能按第二次 Gvr 按鈕 狀態改成 false
-            CanSecondClick = false;
-            // 丟出商品
-            ThrowProduct();
-            // 將 Cart_Collider、FixGazeObj 物件關閉
-            Cart_Collider.SetActive(false);
-            FixGazeObj.SetActive(false);
-
-            // 設定目標物件 (商品) 為 null
-            TargetObj = null;
-            // 設定目標物件之剛體 (商品) 為 null
-            HitRB = null;
+                // 設定目標物件 (商品) 為 null
+                TargetObj = null;
+                // 設定目標物件之剛體 (商品) 為 null
+                HitRB = null;
+            }
         }
     }
 
@@ -149,12 +163,14 @@ public class PickUpAndThrowController : MonoBehaviour {
     /// 丟出商品
     /// </summary>
     private void ThrowProduct() {
-        HitRB.velocity = Camera.main.transform.forward * Throw_Power;
+        HitRB.velocity = GCvrGaze.cam.transform.forward * Throw_Power;
     }
 
     void OnDestroy() {
-        if (instance == this) {
+        // Gvr 按鈕事件
+        GCvrTrigger.OnClick -= GCvrClick;
+
+        if (instance == this)
             instance = null;
-        }
     }
 }
