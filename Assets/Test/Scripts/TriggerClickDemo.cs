9e997298ae906d5970989b78ac7275c82e0e8f9e
titangene <titangene01@gmail.com>
2016-10-11 00:14:34 +0800

    修改：TriggerClickDemo 重構method名稱、新增GazeObj + GazeObjTime

--- Assets/Test/Scripts/TriggerClickDemo.cs
+++ Assets/Test/Scripts/TriggerClickDemo.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 
 public class TriggerClickDemo : MonoBehaviour {
-
     public TextMesh SphereDown_Counter;
     public TextMesh SphereClick_Counter;
     public TextMesh SphereClick_TimeCounter;
@@ -15,44 +14,56 @@ public class TriggerClickDemo : MonoBehaviour {
         GCvrTrigger = MainCamera.GetComponent<GCvrTrigger>();
 
         // Gvr 按鈕事件
-        GCvrTrigger.OnDown      += TiggerDown;
-        GCvrTrigger.OnUp        += TiggerUp;
-        GCvrTrigger.OnClick     += TiggerClick;
-        GCvrTrigger.OnLongClick += TiggerLongClick;
+        GCvrTrigger.OnDown      += GCvrDown;
+        GCvrTrigger.OnUp        += GCvrUp;
+        GCvrTrigger.OnClick     += GCvrClick;
+        GCvrTrigger.OnLongClick += GCvrLongClick;
     }
 
     void LateUpdate() {
         // 目前準心對準的物件
-        SphereClick_TimeCounter.text = GCvrGaze.GetCurrentObjName_Unlimited();
+        PrintGazeObj();
         // 按住 Gvr 按紐時會顯示並計算按了多少時間
         PrintLongClickTime();
     }
 
-    private void TiggerDown(object sender) {
+    private void GCvrDown(object sender) {
         ChangeObjectColor("SphereDown");
-        //Debug.Log("按下按鍵");
+        /*
+        Debug.Log(GCvrGaze.CurrentObj_Range());
+        Debug.Log("目標物件名稱：" + GCvrGaze.CurrentObjName_Range());
+        Debug.Log("目前準心對準物件的碰撞位置：" + GCvrGaze.GetIntersectPosition_Range());
+        Debug.Log("玩家與目標物件的距離：" + GCvrGaze.GetTargetObj_Player_Distance());
+        Debug.Log("是否超過範圍：" + GCvrGaze.IsOverRange());
+        */
+        Debug.Log("目標物件名稱：" + GCvrGaze.GetObjName(GCvrGaze.CurrentObj_Range()));
+        Debug.Log("玩家與目標物件的距離：" + GCvrGaze.GetTargetObj_Player_Distance());
+        // TODO 按下按鍵事件
     }
 
-    private void TiggerUp(object sender) {
+    private void GCvrUp(object sender) {
         ChangeObjectColor("SphereUp");
-        //Debug.Log("放開按鍵");
+
+        // TODO 放開按鍵事件
     }
 
-    private void TiggerClick(object sender) {
+    private void GCvrClick(object sender) {
         ChangeObjectColor("SphereClick");
+
         // 預設是 0，如果偵測是點擊事件就會加 1
         int count = int.Parse(SphereClick_Counter.text) + 1;
         // 將加 1 的數字設定至 Counter 文字物件上
         SphereClick_Counter.text = count.ToString();
-        //Debug.Log("按一下按鍵");
+
+        // TODO 按一下按鍵事件
     }
 
-    private void TiggerLongClick(object sender) {
-        //Debug.Log("按住按鍵");
+    private void GCvrLongClick(object sender) {
+        // TODO 按住按鍵事件
     }
 
     /// <summary>
-    /// 按住 Gvr 按紐時會顯示並計算按了多少時間
+    /// 按住 Gvr 按紐時會顯示：計算按住 Gvr 多少時間
     /// </summary>
     private void PrintLongClickTime() {
         Renderer SphereDown_Counter_Renderer = SphereDown_Counter.GetComponent<Renderer>();
@@ -62,10 +73,20 @@ public class TriggerClickDemo : MonoBehaviour {
             SphereDown_Counter_Renderer.enabled = true;
             // trigger.SecondsHeld() is the number of seconds we've held the trigger down
             SphereDown_Counter.text = GCvrTrigger.ClickTime().ToString("0.0000");
-        } else {
-            // 閃爍SphereDown 內的 Counter 的文字物件
+        } else
+            // 閃爍 SphereDown 內的 Counter 文字物件
             SphereDown_Counter_Renderer.enabled = Time.time % 1 < 0.5;
-        }
+    }
+
+    /// <summary>
+    /// Gaze 任何物件時會顯示：(Gaze 某物件：持續 Gaze 某物件多少時間)
+    /// </summary>
+    private void PrintGazeObj() {
+        // 持續 Gaze 某物件多少時間
+        float count = GCvrGaze.GazeTime();
+        // 將 持續 Gaze 某物件多少時間 資料設定至 TimeCounter 文字物件上：Object：1.2345
+        SphereClick_TimeCounter.text = 
+            GCvrGaze.GetObjName(GCvrGaze.CurrentObj_Infinity()) + "：" + count.ToString("0.0000");
     }
 
     /// <summary>
@@ -85,12 +106,11 @@ public class TriggerClickDemo : MonoBehaviour {
         return new Color(Random.value, Random.value, Random.value);
     }
 
-    // 當掛載該 Script 的物件被銷毀時，可清理一切
     void OnDestroy() {
         // Gvr 按鈕事件
-        GCvrTrigger.OnDown -= TiggerDown;
-        GCvrTrigger.OnUp -= TiggerUp;
-        GCvrTrigger.OnClick -= TiggerClick;
-        GCvrTrigger.OnLongClick -= TiggerLongClick;
+        GCvrTrigger.OnDown -= GCvrDown;
+        GCvrTrigger.OnUp -= GCvrUp;
+        GCvrTrigger.OnClick -= GCvrClick;
+        GCvrTrigger.OnLongClick -= GCvrLongClick;
     }
 }
