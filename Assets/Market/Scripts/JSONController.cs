a855bc5f6bcb6634d181ef6038497a7ff948724f
titangene <titangene01@gmail.com>
2016-10-15 17:56:19 +0800

    新增：JSONController, RandomController

---
+++ Assets/Market/Scripts/JSONController.cs
@@ -0,0 +1,79 @@
+﻿using LitJson;
+using System.IO;
+using System.Text;
+
+public class JSONController {
+    /// <summary>
+    /// 建立目錄
+    /// </summary>
+    public void CreateDirectory(string path) {
+        if (!Directory.Exists(path)) {
+            Directory.CreateDirectory(path);
+        }
+    }
+
+    /// <summary>
+    /// 合併 ReadJson 與 LoadJsonFile 功能。
+    /// ReadJson：讀取 Json 檔，
+    /// LoadJsonFile：讀入 Json 檔的內容
+    /// </summary>
+    public JsonData ReadJson_LoadJsonFile(string fullPath) {
+        string jsonStr = LoadJsonFile(fullPath);
+        return JsonMapper.ToObject(jsonStr);
+    }
+
+    /// <summary>
+    /// 讀取 Json 檔，需搭配 LoadJsonFile() 使用：ReadJson(LoadJsonFile())
+    /// </summary>
+    public JsonData ReadJson(string jsonStr) {
+        return JsonMapper.ToObject(jsonStr);
+    }
+
+    /// <summary>
+    /// 讀入 Json 檔的內容
+    /// </summary>
+    public string LoadJsonFile(string fullPath) {
+        //用來儲存讀入的Json內容
+        StringBuilder sbJson = new StringBuilder();
+        using (StreamReader sr = new StreamReader(fullPath)) {
+            //一次性將資料全部讀入
+            sbJson.Append(sr.ReadToEnd());
+        }
+
+        return sbJson.ToString();
+    }
+
+    /// <summary>
+    /// 刪除 Json 檔
+    /// </summary>
+    public void DeleteFile(string fullPath) {
+        if (File.Exists(fullPath)) {
+            File.Delete(fullPath);
+        }
+    }
+
+    /// <summary>
+    /// 寫入 Json 資料 && 美化 Json
+    /// </summary>
+    public string WriteJsonAndPrettyPrint(JsonData json) {
+        JsonWriter jsonwriter = new JsonWriter();
+        // 美化 Json
+        jsonwriter.PrettyPrint = true;
+        // 縮排大小
+        jsonwriter.IndentValue = 2;
+        JsonMapper.ToJson(json, jsonwriter);
+
+        string str = jsonwriter.ToString();
+        return str;
+    }
+
+    /// <summary>
+    /// 將資料寫入 Json 檔
+    /// </summary>
+    public void OutputJsonFile(JsonData json, string fullPath) {
+        using (StreamWriter sw = new StreamWriter(fullPath, true, Encoding.UTF8)) {
+            string jsonStr = WriteJsonAndPrettyPrint(json);
+            sw.WriteLine(jsonStr);
+        }
+    }
+}
---
+++ Assets/Market/Scripts/RandomController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class RandomController {
+
+    // 亂數 value
+    public Random random;
+
+    /// <summary>
+    /// 產生新的亂數 value
+    /// </summary>
+    public void GeneratorRandom() {
+        // 使用 DateTime.Now.Ticks 可產生不重複的隨機亂數
+        // DateTime.Now.Ticks 是指從 DateTime.MinValue 之後過了多少時間，10000000 為一秒
+        random = new Random((int) DateTime.Now.Ticks);
+    }
+
+    public int UseRandom(int min, int max) {
+        return random.Next(min, max + 1);
+    }
+}
