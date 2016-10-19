45a0d2208661cdfb0561ebf416b4db3b6378295b
titangene <titangene01@gmail.com>
2016-10-19 22:08:18 +0800

    新增：ScoreManager 分數功能，修改：VRMain.prefab 增加分數、時間 UI

---
+++ Assets/Market/Scripts/ScoreManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public Text ScoreText;
+    /// <summary>
+    /// 商品 Tag
+    /// </summary>
+    public string ProductTag = "Product";
+
+    private int score = 0;
+
+    void Awake() {
+        ScoreText = GameObject.FindWithTag("Score").GetComponent<Text>();
+    }
+
+    /// <summary>
+    /// 商品放入購物車時，將商品價格加入分數
+    /// </summary>
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == ProductTag) {
+            int productID = int.Parse(other.name.Substring(7, 4));
+            int json_ID = productID - 1;
+            int Price = int.Parse(ProductManager.Instance.json["product"][json_ID]["price"].ToString());
+            Debug.Log("IN：" + productID + "：" + Price);
+            score += Price;
+            ScoreText.text = "$" + score.ToString();
+        }
+    }
+
+    /// <summary>
+    /// 商品離開購物車時，會將分數減去商品價格
+    /// </summary>
+    void OnTriggerExit(Collider other) {
+        if (other.tag == ProductTag) {
+            int productID = int.Parse(other.name.Substring(7, 4));
+            int json_ID = productID - 1;
+            int Price = int.Parse(ProductManager.Instance.json["product"][json_ID]["price"].ToString());
+            Debug.Log("IN：" + productID + "：" + Price);
+            score -= Price;
+            ScoreText.text = "$" + score.ToString();
+        }
+    }
+}
