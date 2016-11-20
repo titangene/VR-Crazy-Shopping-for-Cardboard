a740fbd7febbffd15cb6c14f36b4cc220b8c2c41
titangene <titangene01@gmail.com>
2016-11-20 21:52:43 +0800

    新增：Game Menu

---
+++ Assets/Market/Scripts/MainPanel/AboutPanel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class AboutPanel : MonoBehaviour {
+    public void OnClickOpenAboutPanel() {
+        gameObject.SetActive(true);
+    }
+
+    public void OnClickCloseAboutPanel() {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Market/Scripts/MainPanel/PlayButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour {
+
+	public void LoadSence() {
+        SceneManager.LoadScene("Test8");
+    }
+}
